f1adfa17af9a54fb62c214e7c65280155f766907
youssefmourad <ymourad@shory.com>
2025-04-29 13:02:34 +0300

    add delete and basic layout

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Touchable,
+  Alert,
+  Pressable,
+} from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert(
+      "Are you sure you want to delete this item?",
+      "It will be gone forever!",
+      [
+        {
+          text: "YES",
+          onPress: () => console.log("Item deleted"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          onPress: () => console.log("Item not deleted"),
+          style: "cancel",
+        },
+      ],
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee!</Text>
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleDelete}
+          activeOpacity={0.5}
+        >
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
+
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -13,8 +52,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  itemContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    borderBottomColor: "#1a759f",
+    borderBottomWidth: 1,
+  },
+  itemText: { fontSize: 18, fontWeight: "200" },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
