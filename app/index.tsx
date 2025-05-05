6905c007a1503225cb594687394862de1f062280
youssefmourad <ymourad@shory.com>
2025-05-05 14:40:53 +0300

    add text input and submit

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,56 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { theme } from "../theme";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted?: boolean;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea", isCompleted: true },
+  { id: "3", name: "Nescafe", isCompleted: true },
+];
 
 export default function Index() {
+  const [inputValue, setInputValue] = useState("");
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+
+  const handleSubmit = () => {
+    if (inputValue) {
+      const newShoppingList = [
+        {
+          id: new Date().toTimeString(),
+          name: inputValue,
+        },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setInputValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Nescafe" isCompleted />
-      <ShoppingListItem />
+      <TextInput
+        placeholder="E.G. Coffee"
+        style={styles.textInput}
+        value={inputValue}
+        onChangeText={setInputValue}
+        onSubmitEditing={handleSubmit}
+        returnKeyType="done"
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem
+          name={item.name}
+          isCompleted={item.isCompleted}
+          key={item.id}
+        />
+      ))}
     </View>
   );
 }
@@ -17,5 +60,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorLightGray,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
